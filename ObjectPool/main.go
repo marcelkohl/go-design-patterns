94fe674697c4ab7285f415b0baa8216b87f58397
Marcel Kohls <marcel.v.kohls@gmail.com>
2021-08-10 13:41:16 +0200

    removed some semicolon

--- ObjectPool/main.go
+++ ObjectPool/main.go
@@ -5,19 +5,19 @@ import "fmt"
 func main() {
   pool := (samplesPool{}).new(2)
 
-  obj, _ := pool.acquire();
+  obj, _ := pool.acquire()
   fmt.Println("got 1st object: ", obj.getId())
 
   fmt.Println("released 1st object: ", obj.getId())
   pool.release(obj)
 
-  obj2, _ := pool.acquire();
+  obj2, _ := pool.acquire()
   fmt.Println("got 2nd object (must be the same as 1st as we released it before acquiring a new one)", obj2.getId())
 
-  obj3, _ := pool.acquire();
+  obj3, _ := pool.acquire()
   fmt.Println("got 3rd object (must be new)", obj3.getId())
 
-  obj4, err := pool.acquire();
+  obj4, err := pool.acquire()
 
   if err != nil {
     fmt.Println(err)
